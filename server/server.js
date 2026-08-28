c8454d34c793bf3f76aba2e7e6abfa19b6ef3f7e
Afroz Shaik <affrozali9342@gmail.com>
2026-08-28 16:05:22 +0530

    Deploying the backend

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use("/api/resume", resumeRoutes);
 app.use("/api/ai", aiRoutes);
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 
   console.log(`Server running on port ${PORT}`);
